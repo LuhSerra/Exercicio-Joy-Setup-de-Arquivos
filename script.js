3453554f7266ca971878feb9de905202e93104ca
LuhSerra <lucia.serrcine@gmail.com>
2024-09-14 13:09:46 -0300

    Setup de Arquivos

---
+++ script.js
@@ -0,0 +1,34 @@
+lconst h1 = document.getElementById('titulo');
+const ul = document.querySelector('ul');
+const a = document.querySelector('a');
+const ol = document.getElementById('lista-ordenada');
+// Adicionando conteúdo ao 'h1' e ao 'a' usando .innerText
+h1.
+h1
+
+h
+innerText = 'Bem-vindo ao meu projeto!';
+a.
+a
+innerText = 'Acesse ProZ Educação';
+
+// Adicionando itens à lista não ordenada (ul) usando .innerHTML
+ul.
+ul
+innerHTML = `
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+`;
+
+
+
+/
+// Adicionando itens à lista ordenada (ol) com links usando .innerHTML
+ol.
+ol
+innerHTML = `
+    <li><a href="https://google.com">Google</a></li>
+    <li><a href="https://github.com">GitHub</a></li>
+    <li><a href="https://mozilla.org">Mozilla</a></li>
+`;
